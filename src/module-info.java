9a209c4fde0db5c26e71154a5e0f8a2b8ba8803d
Suraj <sahusuraj4292@gmail.com>
2022-11-25 22:51:59 +0530

    First commt

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Suraj
+ *
+ */
+module GitHubPractice {
+}
---
+++ src/pages/HomePage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class HomePage {
+public static void main(String[] args) {
+	System.out.println("abc");
+}
+}
